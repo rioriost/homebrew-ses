6f4d4a7d54eabf91c603ebbd9fdcb5d02a39104d
Rio Fujita <rio_github@rio.st>
2026-02-27 17:21:56 +0900

    brew: bump ses formula to 0.0.2

--- Formula/ses.rb
+++ Formula/ses.rb
@@ -1,12 +1,11 @@
 class Ses < Formula
   desc "Speech Event Stream CLI"
   homepage "https://github.com/rioriost/homebrew-ses"
-  url "https://github.com/rioriost/homebrew-ses/releases/download/v0.0.1/ses-0.0.1-macos.tar.gz"
-  sha256 "2a515471c67421b981e20e70e498579a902fa7736939a701149a87e4334cacce"
-  version "0.0.1"
+  url "https://github.com/rioriost/homebrew-ses/releases/download/v0.0.2/ses-0.0.2-macos.tar.gz"
+  sha256 "c321a4354f3a7691820c34e4064841543fdd162928cb4e3a1bcbe9b6f158d822"
+  version "0.0.2"
 
   def install
     bin.install "ses"
   end
 end
-
